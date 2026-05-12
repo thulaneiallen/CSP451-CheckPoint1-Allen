d134af6affc4f5a414825a320a6c248444e6aee4
Thulanei Allen <thulaneiallen@gmail.com>
2026-05-11 21:26:13 -0400

    feat: add JavaScript with console output

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("CSP451 CheckPoint 1 site loaded successfully.");
+
+function displayWelcomeMessage() {
+    console.log("Welcome to my GitHub and version control project.");
+}
+
+displayWelcomeMessage();
